b2e6accf86727646ed462d9e1de45ead77f4fb44
doandesrazvan <razvandoandes@gmail.com>
2026-03-15 12:12:43 +0200

    one more time

--- server.js
+++ server.js
@@ -139,10 +139,11 @@ router.post("/jobs", async (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server running on http://localhost:3000");
 });
-
 
 app.use('.netlify/functions/api', router);
 export const handler = serverless(app);
