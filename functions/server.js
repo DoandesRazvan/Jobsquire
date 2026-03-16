2c876ee089660ed6f436b9e1b8b0e7a4f2ed873e
doandesrazvan <razvandoandes@gmail.com>
2026-03-16 12:21:11 +0200

    -

--- functions/server.js
+++ functions/server.js
@@ -11,7 +11,7 @@ import fs from "fs";
 const app = express();
 
 app.use(express.json({limit: "10mb"}));
-app.use("/", express.static("../public"));
+app.use("/", express.static("public", {root: "/"}));
 
 const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
